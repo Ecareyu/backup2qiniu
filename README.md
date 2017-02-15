b6623baeb08e15fcc98781d08637537d2032b2f7
yuyikai <ykyu@rollong.com>
2017-02-15 15:11:20 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# 备份网站到七牛
+
+由于原来的工具貌似我机器不能玩，不知道为什么，
+上传完成后也没到七牛上，本来决定自己用php搞一个，
+后来发现七牛提供了很好的工具，所以还是不重复造轮子，直接拿来用。
+
+# 依赖
+* qshell [下载](http://devtools.qiniu.com/qshell-v2.0.3.zip)
+* zip
+* tar
+
+
+# 配置
+1. 下载七牛qshell工具
+2. 拷贝config.ini.backup为config.ini
+3. 按照说明修改配置
+
+# 使用
+```bash
+sudo chmod +x backup.sh
+./backup.sh
+```
+
+# 定时备份
+```bash
+crontab -e
+
+# 填入以下,每天凌晨两点执行一次备份,这里可以根据活跃度进行调整
+0 2 * * * /bin/bash /foo/backup.sh
+
+# 我这边网站不活跃，这里配置成每月备份一次
+* * * 1 * /bin/bash /foo/backup.sh
+```
+
+# 参考
+[backuptoqiniu](https://github.com/ccbikai/backuptoqiniu)
---
+++ backup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# 原作者:ccbikai(miantiao.me)
+# 修改者 ecareyu(www.ecareyu.com)
+
+## 以下内容都不要修改 ##
+if [ ! -f ./config.ini ]; then
+    echo "Please create the config.ini file";
+    exit
+fi
+
+# 将配置信息存储session
+source config.ini
+
+if [ ! -f "$QSHELL" ]; then
+    echo "Qshell not found, plese install from this link https://github.com/qiniu/qshell"
+    exit
+fi
+
+# qshell设置用户
+$QSHELL account $QINIU_ACCESS_KEY $QINIU_SECRET_KEY
+
+if [ 0 != $? ]; then
+    echo "Authorization error"
+    exit;
+fi
+
+#精确到秒，统一秒内上传的文件会被覆盖
+NOW=$(date +"%Y%m%d%H%M%S") 
+
+mkdir -p $BACKUP_DIR
+
+# 备份Mysql
+echo "start dump mysql"
+for db_name in $MYSQL_DBS
+do
+    mysqldump -u $MYSQL_USER -h $MYSQL_SERVER -p$MYSQL_PASS $db_name > "$BACKUP_DIR/$BACKUP_NAME-$db_name.sql"
+done
+echo "dump ok"
+
+# 打包
+echo "start tar"
+BACKUP_FILENAME="$BACKUP_NAME-backup-$NOW.zip"
+tarCommand="zip -q -r"
+
+# 判定是否需要密码参数
+if [ -n "$BACKUP_FILE_PASSWD" ]; then
+    tarCommand="$tarCommand -P $BACKUP_FILE_PASSWD"
+fi
+
+$tarCommand $BACKUP_DIR/$BACKUP_FILENAME $BACKUP_DIR/*.sql $BACKUP_SRC
+echo "tar ok"
+
+# 上传,默认100条线程,管它呢
+echo "start upload"
+$QSHELL rput $QINIU_BUCKET $BACKUP_FILENAME $BACKUP_DIR/$BACKUP_FILENAME
+echo "upload ok"
+
+# 清理备份文件
+rm -rf $BACKUP_DIR
+echo "backup clean done"
